a6b0a8d34d73f18e868cd8d4bd783f7886ad84ad
Nguyễn Văn Linh <vanlinh14121@gmail.com>
2023-12-17 01:06:18 +0700

    Home UI

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,71 @@
-import { Layout } from 'antd';
+import { Card, Col, Layout, Row, Typography } from 'antd';
+import { Header } from 'components';
+import _ from 'lodash';
+import { FaPersonRunning } from 'react-icons/fa6';
+import { FiUsers } from 'react-icons/fi';
+import { GiChampions } from 'react-icons/gi';
+import { PiSoccerBallFill } from 'react-icons/pi';
+import { useNavigate } from 'react-router-dom';
 
-const Home = () => (
-  <Layout>
-    <div>Home page</div>
-  </Layout>
-);
+const Home = () => {
+  const navigate = useNavigate();
+
+  const renderCard = (name: string, icon: any, right: boolean = false) => (
+    <Col
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        height: 220,
+      }}
+      span={12}
+    >
+      <Card
+        style={{
+          width: 350,
+          borderRadius: 24,
+          color: 'blue',
+          textAlign: 'center',
+          position: 'absolute',
+          ...(right ? { right: 20 } : { left: 20 }),
+        }}
+        onClick={() => navigate(_.toLower(name))}
+      >
+        <Row justify="center" align="middle">
+          <Col>
+            {icon}
+            <Typography.Title style={{ marginTop: 20 }} level={1}>
+              {name}
+            </Typography.Title>
+          </Col>
+        </Row>
+      </Card>
+    </Col>
+  );
+
+  return (
+    <div>
+      <Header
+        content={
+          <div>
+            <Row justify="center">
+              <Typography.Title level={4}>FootBall League Manager</Typography.Title>
+            </Row>
+          </div>
+        }
+      />
+      <Layout style={{ marginTop: 20 }}>
+        <Row gutter={[10, 50]} justify="center">
+          {renderCard('Leagues', <GiChampions style={{ fontSize: 100 }} />, true)}
+
+          {renderCard('Matches', <PiSoccerBallFill style={{ fontSize: 100 }} />)}
+
+          {renderCard('Teams', <FiUsers style={{ fontSize: 100 }} />, true)}
+
+          {renderCard('Players', <FaPersonRunning style={{ fontSize: 100 }} />)}
+        </Row>
+      </Layout>
+    </div>
+  );
+};
 
 export default Home;
