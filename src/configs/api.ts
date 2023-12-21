256d86ef2b86f31e846f48649f10311e2c3c2efd
Nguyễn Văn Linh <vanlinh14121@gmail.com>
2023-12-21 21:07:57 +0700

    update server

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,4 +1,7 @@
 export const BACKEND_URI = 'http://localhost';
 export const PORT = 5053;
 
-export const BACKEND = `${BACKEND_URI}:${PORT}`;
+export const BACKEND =
+  process.env.REACT_APP_STAGE === 'development'
+    ? `${BACKEND_URI}:${PORT}`
+    : 'https://football-backend-u8tj.onrender.com/';
